1dab2a903640a4f8e89cd88ca2e5a307a04c844f
Assaf <assaf@post.bgu.ac.il>
2020-04-21 19:21:33 +0300

    added dev

---
+++ dev/src/com/company/LogicLayer/Product.java
@@ -0,0 +1,15 @@
+package com.company.LogicLayer;
+
+public class Product{
+
+    private String location;
+    private String id;
+    private Enum<int> isInStorage;
+    private Date expirationDate;
+    private boolean isDamaged;
+
+
+    public Product(String){
+
+    }
+}
