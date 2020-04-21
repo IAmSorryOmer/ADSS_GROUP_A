19c7c848414dd7a56392fbf7f35971d21f24ddf9
Assaf <assaf@post.bgu.ac.il>
2020-04-21 21:02:15 +0300

    basic project

---
+++ dev/src/com/company/LogicLayer/CategoryController.java
@@ -0,0 +1,4 @@
+package com.company.LogicLayer;
+
+public class CategoryController {
+}
---
+++ dev/src/com/company/LogicLayer/ProductDetailsController.java
@@ -0,0 +1,8 @@
+package com.company.LogicLayer;
+
+import java.util.List;
+
+public class ProductDetailsController {
+    private static List<ProductDetails> productDetails;
+
+}
